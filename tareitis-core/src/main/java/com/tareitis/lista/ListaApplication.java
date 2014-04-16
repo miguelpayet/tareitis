29a7ee0217509614f031e86f0380d7916077061b
Miguel Payet <miguelpayet@gmail.com>
2014-04-16 11:40:56 -0500

    Corregir errores

--- tareitis-core/src/main/java/com/tareitis/lista/ListaApplication.java
+++ tareitis-core/src/main/java/com/tareitis/lista/ListaApplication.java
@@ -1,30 +1,37 @@
 package com.tareitis.lista;
 
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
-import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class ListaApplication extends Application<ListaConfiguration> {
-	private final HibernateBundle<ListaConfiguration> hibernateBundle = new ListaApplication();
-	private final MiShiroBundle<ListaConfiguration> shiro = new ListaApplication(this);
-
+	
 	public static void main(String[] args) throws Exception {
 		new ListaApplication().run(args);
 	}
+	
+	private final HibernateBundle<ListaConfiguration> hibernate = new HibernateBundle<ListaConfiguration>(Lista.class,
+			Tarea.class) {
+		public DataSourceFactory getDataSourceFactory(ListaConfiguration configuration) {
+			return configuration.getDataSourceFactory();
+		}
+	};
+
+	private final MiShiroBundle<ListaConfiguration> shiro = new MiShiroBundle<ListaConfiguration>() {};
 
 	public String getName() {
 		return "lista";
 	}
 
 	public void initialize(Bootstrap<ListaConfiguration> bootstrap) {
-		bootstrap.addBundle(this.hibernateBundle);
+		bootstrap.addBundle(this.hibernate);
 		bootstrap.addBundle(this.shiro);
 	}
 
 	public void run(ListaConfiguration configuration, Environment environment) {
-		TareaDAO tareaDAO = new TareaDAO(this.hibernateBundle.getSessionFactory());
+		TareaDAO tareaDAO = new TareaDAO(this.hibernate.getSessionFactory());
 		ListaResource resource = new ListaResource(tareaDAO);
 		environment.jersey().register(resource);
 	}
